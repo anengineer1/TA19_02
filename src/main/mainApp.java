2d5477f2be6deefb67b7cbfd58bba0b2f28df388
Francisco Parra <franparpe@disroot.org>
2023-04-02 18:19:51 +0200

    Refactor

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -5,7 +5,6 @@ import gui_components.MainWindow;
 public class mainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		MainWindow my_window = new MainWindow();
 	}
 
